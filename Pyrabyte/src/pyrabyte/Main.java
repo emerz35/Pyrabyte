8eefe4f7f7d9d0387bad7fa3bcf606bd0db43d1b
Charlie Hands <Charlie Hands@DESKTOP-7OJDCUU.Home>
2020-04-25 22:34:12 +0100

    Added Game Loop

--- Pyrabyte/src/pyrabyte/Main.java
+++ Pyrabyte/src/pyrabyte/Main.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pyrabyte;
+
+import java.awt.Canvas;
 
 /**
  *
  * @author Charlie Hands
  */
-public class Main {
-
+public class Main extends Canvas{
+    
+    public static int WIDTH = 1240, HEIGHT = 800;
+    
+    Window window;
+    Thread renderThread;
+    Renderer renderer;
+    
+    public Main(String ip, int port, boolean isServer){
+        
+        
+        window  = new Window("Pyrabyte", WIDTH,HEIGHT, this);
+    }
+    
+    public void start(){
+        createBufferStrategy(4);
+        renderer = new Renderer(this);
+        renderThread = new Thread(renderer);
+        renderThread.setDaemon(true);
+        renderThread.start();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Main("", 0, true);
     }
     
 }
---
+++ Pyrabyte/src/pyrabyte/Player.java
@@ -0,0 +1,11 @@
+
+package pyrabyte;
+
+/**
+ *
+ * @author Charlie Hands
+ */
+public class Player {
+    
+    
+}
