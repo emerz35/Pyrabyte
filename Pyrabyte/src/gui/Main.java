12af81a07d93a0000caff4b0b8e382e9824711af
Adam <Adam@DESKTOP-N1S4P09.broadband>
2020-04-29 22:34:30 +0100

    Custom Serializer

--- Pyrabyte/src/gui/Main.java
+++ Pyrabyte/src/gui/Main.java
@@ -71,7 +71,7 @@ public class Main extends Canvas{
         Scanner scan = new Scanner(System.in);
 
         //System.out.println("Enter ip uwu:");
-        String ip = scan.nextLine();
+        //String ip = scan.nextLine();
         
         try{
 
